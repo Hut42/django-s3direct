9c23de3ac192636f0be4941f422b97cdfdeced8a
Neil Hepburn <neil@hut42.co.uk>
2017-10-06 13:22:22 +0100

    Updates

--- s3direct/fields.py
+++ s3direct/fields.py
@@ -6,7 +6,10 @@ from s3direct.widgets import S3DirectWidget
 class S3DirectField(Field):
     def __init__(self, *args, **kwargs):
         dest = kwargs.pop('dest', None)
-        self.widget = S3DirectWidget(dest=dest)
+        path = ''
+        if 'path' in kwargs:
+            path = kwargs.pop('path', '')
+        self.widget = S3DirectWidget(dest=dest, path=path)
         super(S3DirectField, self).__init__(*args, **kwargs)
 
     def get_internal_type(self):
